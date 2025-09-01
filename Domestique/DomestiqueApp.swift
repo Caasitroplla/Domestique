596c8a4250c4b3ebacf491dd1c9c76c9adb52dda
isaac <caasitroplla@icloud.com>
2025-09-01 10:59:52 +0100

    Initial Commit

---
+++ Domestique/DomestiqueApp.swift
@@ -0,0 +1,17 @@
+//
+//  DomestiqueApp.swift
+//  Domestique
+//
+//  Created by Issac Allport on 01/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct DomestiqueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
